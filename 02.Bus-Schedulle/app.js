1de7be099d2ca87a828cad66cf801049fc223fc3
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-08-14 14:09:29 +0300

    Commit 02.Bus-Schedule with fetch, then, catch.

--- 02.Bus-Schedulle/app.js
+++ 02.Bus-Schedulle/app.js
@@ -1,16 +1,39 @@
 function solve() {
-    function depart() {
-        // TODO: 
-    }
+  const departButton = document.querySelector("#depart");
+  const arriveButton = document.querySelector("#arrive");
+  const busInfoBox = document.querySelector("#info .info");
 
-    async function arrive() {
-        // TODO:
-    }
+  let busStopInfo = {
+    name: "",
+    next: "depot",
+  };
 
-    return {
-        depart,
-        arrive
-    };
+  function depart() {
+    fetch(`http://localhost:3030/jsonstore/bus/schedule/${busStopInfo.next}`)
+      .then((res) => res.json())
+      .then((busStop) => {
+        busStopInfo = busStop;
+        departButton.disabled = true;
+        arriveButton.disabled = false;
+        busInfoBox.textContent = `Next stop: ${busStopInfo.name}`;
+      })
+      .catch(() => {
+        departButton.disabled = true;
+        arriveButton.disabled = true;
+        busInfoBox.textContent = `Error`;
+      });
+  }
+
+  async function arrive() {
+    departButton.disabled = false;
+    arriveButton.disabled = true;
+    busInfoBox.textContent = `Arriving at: ${busStopInfo.name}`;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
