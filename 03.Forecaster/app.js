016b40319809bda6d825f7c36db7ccdf6690f97a
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-08-15 11:24:58 +0300

    Commit with part of 03.Forecaster.

--- 03.Forecaster/app.js
+++ 03.Forecaster/app.js
@@ -1,5 +1,34 @@
 function attachEvents() {
-    // TODO:
+  document
+    .querySelector("#submit")
+    .addEventListener("click", getWeatherDataForLocation);
+}
+
+async function getWeatherDataForLocation() {
+  const locationName = document.querySelector("#location").value;
+  const locationResponse = await (
+    await fetch("http://localhost:3030/jsonstore/forecaster/locations")
+  ).json();
+
+  const location = locationResponse.find(
+    (l) => l.name.toLowerCase() === locationName.toLowerCase()
+  );
+
+  const currentConditionsResponse = await (
+    await fetch(
+      `http://localhost:3030/jsonstore/forecaster/today/${location.code}`
+    )
+  ).json();
+
+  console.log(currentConditionsResponse);
+
+  const threeDayForecastResponse = await (
+    await fetch(
+      `http://localhost:3030/jsonstore/forecaster/upcoming/${location.code}`
+    )
+  ).json();
+
+  console.log(threeDayForecastResponse);
 }
 
 attachEvents();
