669dc796dddb21d99983ac07a10a11aae37c7da8
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-08-16 18:57:31 +0300

    Commit with part of 04.Blog

--- 04.Blog/app.js
+++ 04.Blog/app.js
@@ -1,5 +1,12 @@
 function attachEvents() {
-    // TODO:
+  const buttonLoadPost = document.querySelector("#btnLoadPosts");
+  buttonLoadPost.addEventListener("click", loadPosts);
+}
+
+async function loadPosts() {
+  const result = await (
+    await fetch("http://localhost:3030/jsonstore/blog/posts")
+  ).json();
 }
 
 attachEvents();
